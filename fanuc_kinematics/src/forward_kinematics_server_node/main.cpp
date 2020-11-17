b510e9c329bd5dad01c9eba5e94d990d55376b79
v8p1197 <v.petrone6@studenti.unisa.it>
2020-11-17 20:43:02 +0100

    First implementation of FK server

--- fanuc_kinematics/src/forward_kinematics_server_node/main.cpp
+++ fanuc_kinematics/src/forward_kinematics_server_node/main.cpp
@@ -1,10 +1,31 @@
 #include <ros/ros.h>
+#include <moveit/robot_state/robot_state.h>
+#include <moveit/robot_state/conversions.h>
+#include <moveit/robot_model_loader/robot_model_loader.h>
+
 #include "fanuc_kinematics_msgs/ComputeFK.h"
 
 
-bool compute_fk(fanuc_kinematics_msgs::ComputeFK::Request& request,
-                fanuc_kinematics_msgs::ComputeFK::Response& response)
-{
+bool compute_fk(fanuc_kinematics_msgs::ComputeFK::Request& request, fanuc_kinematics_msgs::ComputeFK::Response& response) {    
+    // Load robot model
+    robot_model_loader::RobotModelLoader robotModelLoader("robot_description");
+    robot_model::RobotModelPtr kinematicModel = robotModelLoader.getModel();
+    robot_state::RobotStatePtr robotState(new robot_state::RobotState(kinematicModel));
+
+    // Get request parameters
+    std::vector<std::string> links = request.fk_link_names;
+    bool success = moveit::core::robotStateMsgToRobotState(request.robot_state, *robotState);
+    if (!success) return false;
+
+    // Iterate over links
+    for(std::size_t i = 0; i < links.size(); ++i) {
+        std::string link = links[i];
+        // Compute FK for each link
+        const Eigen::Isometry3d& endEffectorState = robotState->getGlobalLinkTransform(link);
+        ROS_INFO_STREAM("TEST Translation: \n" << endEffectorState.translation() << "\n");
+        ROS_INFO_STREAM("TEST Rotation: \n" << endEffectorState.rotation() << "\n");
+    }
+
     return true;
 }
 
