6e147f3690c95a76efb5f2875b54cafb6eeaf7af
v8p1197 <v.petrone6@studenti.unisa.it>
2020-11-17 23:24:11 +0100

    Point 1 and 2 completed

--- fanuc_kinematics/src/forward_kinematics_client_node/main.cpp
+++ fanuc_kinematics/src/forward_kinematics_client_node/main.cpp
@@ -15,10 +15,13 @@ int main(int argc, char** argv) {
     robot_model_loader::RobotModelLoader robotModelLoader("robot_description");
     robot_model::RobotModelPtr robotModel = robotModelLoader.getModel();
     robot_state::RobotStatePtr robotState(new robot_state::RobotState(robotModel));
+    robotState->setToDefaultValues();
     std::string modelFrame = robotModel->getModelFrame();
     std::string modelGroup = robotModel->getSRDF()->getGroups()[0].name_;
     robot_state::JointModelGroup* jointModelGroup = robotModel->getJointModelGroup(modelGroup);
-    std::vector<std::string> linkNames = jointModelGroup->getLinkModelNames();    
+    std::vector<std::string> linkNames = jointModelGroup->getLinkModelNames();  
+    const std::vector<std::string>& jointNames = jointModelGroup->getVariableNames();
+    std::vector<double> jointValues;  
 
     // Get the service
     std::string serviceName;
@@ -32,20 +35,34 @@ int main(int argc, char** argv) {
     service.request.fk_link_names = { linkNames.back() };
     moveit::core::robotStateToRobotStateMsg(*robotState, service.request.robot_state);
 
-    // Post request
+    // Loop
     ros::Rate rate = ros::Rate(0.1);
     while (true) {
+        // Sleep
         rate.sleep();
+        // Post request
         if (client.call(service)) {
+            // Check if all FKs were computed and returned
+            if (service.request.fk_link_names.size() != service.response.fk_link_names.size()) {
+                ROS_WARN("Not all FKs computed!");
+            }
+            // Iterate over returned links
             for (std::size_t i = 0; i < service.response.fk_link_names.size(); ++i) {
                 std::string link = service.response.fk_link_names[i];
-                ROS_INFO_STREAM("******* Trasform " << modelFrame << " to " << link << " ******* \n"
-                                "Translation: \n" << service.response.pose_stamped[i].pose.position << "\n"
-                                "Rotation: \n" << service.response.pose_stamped[i].pose.orientation << "\n");
+                ROS_INFO_STREAM("******* Forward Kinematics from " << modelFrame << " to " << link << " *******\n" <<
+                                service.response.pose_stamped[i].pose);
             }    
         } else {
             ROS_ERROR("Failed to call service %s", serviceName.c_str());
         }
+        // Update request for new configuration
+        robotState->setToRandomPositions(jointModelGroup);
+        ROS_INFO_STREAM("******* New configuration: *******");
+        robotState->copyJointGroupPositions(jointModelGroup, jointValues);
+        for (std::size_t i = 0; i < jointNames.size(); ++i) {
+            ROS_INFO("Joint %s: %f", jointNames[i].c_str(), jointValues[i]);
+        }        
+        moveit::core::robotStateToRobotStateMsg(*robotState, service.request.robot_state);        
     }
 
     return 0;
