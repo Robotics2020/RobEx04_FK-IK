f89f7fa71c20c97ac3a36b23fd92e76d20d3be8a
v8p1197 <v.petrone6@studenti.unisa.it>
2020-11-17 21:36:49 +0100

    First implementation of FK client

--- fanuc_kinematics/src/forward_kinematics_client_node/main.cpp
+++ fanuc_kinematics/src/forward_kinematics_client_node/main.cpp
@@ -1,4 +1,8 @@
 #include <ros/ros.h>
+#include <moveit/robot_state/robot_state.h>
+#include <moveit/robot_state/conversions.h>
+#include <moveit/robot_model_loader/robot_model_loader.h>
+#include "fanuc_kinematics_msgs/ComputeFK.h"
 
 
 int main(int argc, char** argv) {
@@ -7,10 +11,33 @@ int main(int argc, char** argv) {
     ros::init(argc, argv, "fanuc_FK_client");
     ros::NodeHandle nodeHandle;
 
-    // Start the main loop
-    ros::Rate rate(0.1);
-    while (nodeHandle.ok()) {
-        rate.sleep();
+    // Load robot model
+    robot_model_loader::RobotModelLoader robotModelLoader("robot_description");
+    robot_model::RobotModelPtr robotModel = robotModelLoader.getModel();
+    robot_state::RobotStatePtr robotState(new robot_state::RobotState(robotModel));
+    std::string modelFrame = robotModel->getModelFrame();
+    std::string modelGroup = robotModel->getSRDF()->getGroups()[0].name_;
+    robot_state::JointModelGroup* jointModelGroup = robotModel->getJointModelGroup(modelGroup);
+    std::vector<std::string> linkNames = jointModelGroup->getLinkModelNames();    
+
+    // Get the service
+    std::string serviceName;
+    ros::param::get("FK_service", serviceName);
+    ros::ServiceClient client = nodeHandle.serviceClient<fanuc_kinematics_msgs::ComputeFK>(serviceName);
+
+    // Create request
+    fanuc_kinematics_msgs::ComputeFK service;
+    service.request.header.stamp = ros::Time::now();
+    service.request.header.frame_id = modelFrame;
+    service.request.fk_link_names = { linkNames.back() };
+    moveit::core::robotStateToRobotStateMsg(*robotState, service.request.robot_state);
+
+    // Post request
+    ros::Rate(0.1).sleep();
+    if (client.call(service)) {
+        return 0;
+    } else {
+        ROS_ERROR("Failed to call service %s", serviceName.c_str());
     }
 
     return 0;
--- fanuc_kinematics/src/forward_kinematics_server_node/main.cpp
+++ fanuc_kinematics/src/forward_kinematics_server_node/main.cpp
@@ -2,15 +2,14 @@
 #include <moveit/robot_state/robot_state.h>
 #include <moveit/robot_state/conversions.h>
 #include <moveit/robot_model_loader/robot_model_loader.h>
-
 #include "fanuc_kinematics_msgs/ComputeFK.h"
 
 
 bool compute_fk(fanuc_kinematics_msgs::ComputeFK::Request& request, fanuc_kinematics_msgs::ComputeFK::Response& response) {    
     // Load robot model
     robot_model_loader::RobotModelLoader robotModelLoader("robot_description");
-    robot_model::RobotModelPtr kinematicModel = robotModelLoader.getModel();
-    robot_state::RobotStatePtr robotState(new robot_state::RobotState(kinematicModel));
+    robot_model::RobotModelPtr robotModel = robotModelLoader.getModel();
+    robot_state::RobotStatePtr robotState(new robot_state::RobotState(robotModel));
 
     // Get request parameters
     std::vector<std::string> links = request.fk_link_names;
@@ -31,11 +30,16 @@ bool compute_fk(fanuc_kinematics_msgs::ComputeFK::Request& request, fanuc_kinema
 
 
 int main(int argc, char** argv) {
+    // Init node
     ros::init(argc, argv, "fanuc_FK_server");
     ros::NodeHandle nodeHandle;
+    
+    // Start the service
     std::string serviceName;
     ros::param::get("FK_service", serviceName);
     ros::ServiceServer service = nodeHandle.advertiseService(serviceName, compute_fk);
+    
+    // Loop
     ros::spin();
     return 0;
 }
